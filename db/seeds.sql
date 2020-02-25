33889afb662422f76763fecec5e4e4086b4be8f0
jpm <jpmckenna90@gmail.com>
2020-02-24 22:35:35 -0500

    Built initial directory structure.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers VALUES ("Beyond Burger", true),
+("Veggie Burger", true), ("Double Bacon Cheeseburger", false),
+("Turkey Burger", false), ("Black Bean Burger", true);
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+  selectAll: function(){
+    orm.selectAll({
+
+    })
+  },
+  insertOne: function(){
+    orm.insertOne({
+
+    })
+  },
+  updateOne: function(){
+    orm.updateOne({
+
+    })
+  }
+}
+
+module.exports = burger;
