02936dfe447089ef55e3c1b399f36f01b2255a93
jpm <jpmckenna90@gmail.com>
2020-02-26 20:13:27 -0500

    Implemented burger-block partial, succesfully displaying burgers to main view.

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -10,7 +10,32 @@ router.get("/", function(req, res){
     console.log(hbsObject);
     res.render("index", hbsObject)
   })
-  // res.render("index");
+})
+
+router.post("/api/burgers", function(req, res){
+  burger.insertOne([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result){
+    res.json({id: result.insertId});
+  })
+})
+
+router.put("/api/burgers/:id", function(req, res){
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result){
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  })
 })
 
 module.exports = router;
